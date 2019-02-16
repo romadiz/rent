2615eeba7ec7810a20007075d6d25730f323b88e
romadiz <rodizma@gmail.com>
2019-02-16 20:15:07 +0100

    Init commit

---
+++ src/main/java/com/rent/rank/rentRank/RentRankApplication.java
@@ -0,0 +1,14 @@
+package com.rent.rank.rentRank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentRankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentRankApplication.class, args);
+	}
+
+}
+
